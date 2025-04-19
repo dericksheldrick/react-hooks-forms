fc1697211ea1d2742160251ffd37bb64f5647285
Derick Sheldrick <sheldrickderick9@gmail.com>
2025-04-19 08:46:28 +0300

    form submission

--- src/components/Form.js
+++ src/components/Form.js
@@ -4,10 +4,19 @@ function Form() {
   const [firstName, setFirstName] = useState("John");
   const [lastName, setLastName] = useState("Henry");
 
+  function handleFirstNameChange(event){
+    setFirstName(event.targt.value);
+  }
+
+  function handleLastNameChange(event){
+    setLastName(event.targt.value);
+  }
+
+
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" value={firstName} onChange={handleFirstNameChange}/>
+      <input type="text" value={lastName}  onChange={handleLastNameChange}/>
       <button type="submit">Submit</button>
     </form>
   );
